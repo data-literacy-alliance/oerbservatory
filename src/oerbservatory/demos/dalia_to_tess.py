3e6a050058e60f250cc234e35ed35364ad966bc6
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-02 16:54:45 +0100

    Add DALIA to TeSS demo

---
+++ src/oerbservatory/demos/dalia_to_tess.py
@@ -0,0 +1,51 @@
+"""Demonstrate converting DALIA DIF v1.3 to TeSS."""
+
+from dalia_dif.dif13.rdf import get_discipline_label
+from dalia_dif.dif13 import EducationalResourceDIF13
+from tess_downloader import LearningMaterial, Topic
+from dalia_dif.dif13 import read_dif13
+import json
+
+
+def main() -> None:
+    """Demonstrate converting DALIA DIF v1.3 to TeSS."""
+    url = "https://github.com/data-literacy-alliance/dalia-curation/raw/refs/heads/main/curation/NFDI4Chem.csv"
+    tess_oers = [
+        tess_oer
+        for dalia_oer in read_dif13(url)
+        if (tess_oer := _from_dalia_dif13(dalia_oer))
+    ]
+    with open("/Users/cthoyt/Desktop/tess_from_dalia.json", "w") as file:
+        json.dump(
+            [tess_oer.model_dump(exclude_none=True, exclude_unset=True) for tess_oer in tess_oers],
+            file,
+            indent=2,
+            ensure_ascii=False,
+        )
+
+
+def _from_dalia_dif13(oer: EducationalResourceDIF13) -> LearningMaterial | None:
+    if not oer.description:
+        print(oer.title, "has no description")
+        return None
+
+    return LearningMaterial(
+        slug=str(oer.uuid),  # the slug is the
+        title=oer.title,
+        url=oer.links[0],
+        description=oer.description,
+        keywords=oer.keywords,
+        # resource_type, # TODO needs the DALIA-TeSS mapping
+        # other_types
+        scientific_topics=[
+            Topic(
+                label=get_discipline_label(discipline),
+                uri=str(discipline),
+            )
+            for discipline in oer.disciplines
+        ],
+    )
+
+
+if __name__ == '__main__':
+    main()
